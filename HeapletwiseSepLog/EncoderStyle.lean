8fd1875b17230de95152541782d6fc73e2df13fc
Samuel Gruetter <saemi472@gmail.com>
2026-03-29 18:27:21 +0200

    RelationalStyle and EncoderStyle can define Heaplet the same way

--- HeapletwiseSepLog/EncoderStyle.lean
+++ HeapletwiseSepLog/EncoderStyle.lean
@@ -4,9 +4,44 @@ abbrev Address := UInt64
 abbrev Byte := UInt8
 abbrev Mem := Std.ExtHashMap Address Byte
 
-inductive OnHeapAt
-  {T : Type} (encode : T -> Address -> Option Mem) (addr : Address) : Type
+structure Heaplet {Val : Type}
+  (encode : Val -> Address -> Option Mem) (val: Val) (addr : Address) : Type
 where
-  | mk (val : T) (mem : Mem) (connection : encode val addr = some mem)
+  mem : Mem
+  connection : encode val addr = some mem
 
-infix:80 " @ " => OnHeapAt /- TODO will this class with @make_implicit_args_explicit ? -/
+structure MyRecord where
+  x: UInt64
+  y: UInt64
+
+section Sketch
+
+variable (myRecord : MyRecord -> Address -> Option Mem)
+
+variable (uint : Nat -> Nat -> Address -> Option Mem)
+
+variable (array : forall {E : Type}, (E -> Address -> Option Mem) -> Nat ->
+  List Nat -> Address -> Option Mem)
+ /- TODO where to put elem size/offset between elements? Infer using typeclass? -/
+
+variable (Instr : Type)
+
+variable (wp : List Instr -> Mem -> (Mem -> Prop) -> Prop)
+
+variable (incFieldX : List Instr)
+
+theorem incFieldX_correct (a_r : Address) (a_vs : Address)
+  (r : MyRecord) (vs : List Nat)
+  (h_r : Heaplet myRecord r a_r)
+  (h_vs : Heaplet (array (uint 8) 16) vs a_vs)
+  (boundX : r.x.toNat < UInt64.size - 1)
+  (m : Mem)
+  (D : m.splits [h_r.mem, h_vs.mem])
+  (post : Mem -> Prop):
+  (forall h_r' : Heaplet myRecord { r with x := r.x + 1 } a_r,
+    m.splits [h_r'.mem, h_vs.mem] ->
+    post m) ->
+  wp incFieldX m post
+  := by sorry
+
+end Sketch
--- HeapletwiseSepLog/RelationalStyle.lean
+++ HeapletwiseSepLog/RelationalStyle.lean
@@ -4,9 +4,44 @@ abbrev Address := UInt64
 abbrev Byte := UInt8
 abbrev Mem := Std.ExtHashMap Address Byte
 
-inductive OnHeapAt
-  {T : Type} (relate : T -> Address -> Mem -> Prop) (addr : Address) : Type
+structure Heaplet {Val : Type}
+  (relate : Val -> Address -> Mem -> Prop) (val: Val) (addr : Address) : Type
 where
-  | mk (val : T) (mem : Mem) (connection : relate val addr mem)
+  mem : Mem
+  connection : relate val addr mem
 
-infix:80 " @ " => OnHeapAt /- TODO will this class with @make_implicit_args_explicit ? -/
+structure MyRecord where
+  x: UInt64
+  y: UInt64
+
+section Sketch
+
+variable (myRecord : MyRecord -> Address -> Mem -> Prop)
+
+variable (uint : Nat -> Nat -> Address -> Mem -> Prop)
+
+variable (array : forall {E : Type}, (E -> Address -> Mem -> Prop) -> Nat ->
+  List Nat -> Address -> Mem -> Prop)
+ /- TODO where to put elem size/offset between elements? Infer using typeclass? -/
+
+variable (Instr : Type)
+
+variable (wp : List Instr -> Mem -> (Mem -> Prop) -> Prop)
+
+variable (incFieldX : List Instr)
+
+theorem incFieldX_correct (a_r : Address) (a_vs : Address)
+  (r : MyRecord) (vs : List Nat)
+  (h_r : Heaplet myRecord r a_r)
+  (h_vs : Heaplet (array (uint 8) 16) vs a_vs)
+  (boundX : r.x.toNat < UInt64.size - 1)
+  (m : Mem)
+  (D : m.splits [h_r.mem, h_vs.mem])
+  (post : Mem -> Prop):
+  (forall h_r' : Heaplet myRecord { r with x := r.x + 1 } a_r,
+    m.splits [h_r'.mem, h_vs.mem] ->
+    post m) ->
+  wp incFieldX m post
+  := by sorry
+
+end Sketch
